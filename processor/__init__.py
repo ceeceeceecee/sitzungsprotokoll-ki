52ebdd4a5119694f1a360c70b6d8065a627cd291
Chris Cole <chrcup@proton.me>
2026-05-02 21:29:39 +0200

    Add requirements, Dockerfile, docker-compose, gitignore

--- processor/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Processor-Modul für Sitzungsprotokoll-KI.
-"""
-from .transcriber import AudioTranscriber
-from .protocol_generator import ProtocolGenerator
-from .document_exporter import DocumentExporter
-
-__all__ = ["AudioTranscriber", "ProtocolGenerator", "DocumentExporter"]
